ae2d9c12b78c2afafc9aa14b33169ccaaa0f1591
Gordon Meade <gordonmeade@gmail.com>
2024-02-27 14:55:29 +0000

    Add CSS to Project

--- codestar/urls.py
+++ codestar/urls.py
@@ -22,8 +22,11 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("blog.urls"), name="blog-urls"),
+    
+   
     
     # Other paths...
 ]
